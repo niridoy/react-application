17fab9dde6f13f1a6fd128aa4508b60ecd624edb
Nazmul Islam <nazmul_islam@feemaatech.com>
2020-02-23 18:25:35 +0600

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');  
+const HtmlWebpackPlugin = require('html-webpack-plugin');  
+  
+module.exports = {  
+   entry: './main.js',
+   watch: true,  
+   output: {  
+      path: path.join(__dirname, '/bundle'),  
+      filename: 'index_bundle.js'  
+   },  
+   devServer: {  
+      inline: true,  
+      port: 8090  
+   },  
+   module: {  
+      rules: [  
+         {  
+            test: /\.jsx?$/,  
+            exclude: /node_modules/,  
+        use: {  
+              loader: "babel-loader",  
+            }  
+         }  
+      ]  
+   },  
+   plugins:[  
+      new HtmlWebpackPlugin({  
+         template: './index.html'  
+      })  
+   ]  
+}  
